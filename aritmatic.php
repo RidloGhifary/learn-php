bc1519f12c8cbde1b0851c8a6ebade0c8d9f56fb
RidloGhifary <ridloghfry@gmail.com>
2024-09-14 16:45:40 +0700

    learn: php mathematic operation

---
+++ aritmatic.php
@@ -0,0 +1,19 @@
+<?php
+
+$result = 10 + 10;
+var_dump($result);
+
+$result = 10 - 5;
+var_dump($result);
+
+$result = 10 * 10;
+var_dump($result);
+
+$result = 10 / 5;
+var_dump($result);
+
+$result = 10 % 5;
+var_dump($result);
+
+$result = 10 ** 5;
+var_dump($result);
