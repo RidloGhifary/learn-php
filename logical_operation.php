83b302d13d3e369fad76ea87ba849bb49a5e8f1c
RidloGhifary <ridloghfry@gmail.com>
2024-09-14 17:00:11 +0700

    learn: php logical operation

---
+++ logical_operation.php
@@ -0,0 +1,12 @@
+<?php
+
+// && true if both is a true value
+// and true if both is a true value
+// || true if one of them is a true value
+// or true if one of them is a true value
+// ! true is value is a falsy value
+// xor true if one of em true but not both
+
+var_dump(true xor false);
+var_dump(true xor true);
+var_dump(!true);
