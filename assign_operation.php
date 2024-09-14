1cd73bab40b1e13a55b142b3570154b2a6499c44
RidloGhifary <ridloghfry@gmail.com>
2024-09-14 16:53:23 +0700

    learn: php comparison & assign operation

---
+++ assign_operation.php
@@ -0,0 +1,11 @@
+<?php
+
+$total = 0;
+
+$chicken = 10000;
+$water = 3000;
+
+$total += $chicken;
+$total += $water;
+
+var_dump($total);
---
+++ comparison_operation.php
@@ -0,0 +1,15 @@
+<?php
+
+// == true if both has same value, converting data type
+// === true is both has the same value and data type
+// != true if one of it does not have the same value
+// !== true if one of it does not have the same value and the same data type
+// < true if first value less than second value
+// <= true if first value less or same with second value
+// > true if first value more than second value
+// >= true if first value more or same with second value
+
+var_dump(10 == "10");
+var_dump(10 === "10");
+var_dump(10 < "10");
+var_dump(10 <= "10");
