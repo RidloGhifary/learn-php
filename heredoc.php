06ff012b391292bd2268ae3d2a6d3ff8cdc16e1b
RidloGhifary <ridloghfry@gmail.com>
2024-09-14 16:14:47 +0700

    learn: php heredoc

---
+++ heredoc.php
@@ -0,0 +1,9 @@
+<?php
+
+// Heredoc is a feature that can do a long string, that allow to manually enter, tab etc
+
+echo <<<RIDLO
+this is an example of a string that
+very very very long so you could even read
+it propertly and be lazy
+RIDLO;
