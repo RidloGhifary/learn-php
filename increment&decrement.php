d3fae832599e7b76ce7c57bbf08829c7fd972a49
RidloGhifary <ridloghfry@gmail.com>
2024-09-14 17:04:04 +0700

    learn: php increment & decrement operation

---
+++ increment&decrement.php
@@ -0,0 +1,12 @@
+<?php
+
+// $a++ post increment, return the value then increment 1
+// ++$a pre increment, increment the return the value
+// $a-- pre decrement
+// --$a pre decrement
+
+$a = 10;
+$b = ++$a;
+
+var_dump($a);
+var_dump($b);
