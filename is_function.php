7d9c431ba41cd45374edb2a483c6160dd09416d5
RidloGhifary <ridloghfry@gmail.com>
2024-09-15 13:54:50 +0700

    learn: php is function

---
+++ is_function.php
@@ -0,0 +1,12 @@
+<?php
+
+// php has many is function that used to check data type
+// reference : https://www.php.net/manual/en/ref.var.php 
+
+$value = "ridlo";
+
+var_dump(is_array($value));
+var_dump(is_bool($value));
+var_dump(is_callable($value));
+var_dump(is_float($value));
+var_dump(is_string($value));
