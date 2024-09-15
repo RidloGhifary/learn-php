f81ce0be3a59f9d5d4a9c836c24ab53718fd8d9b
RidloGhifary <ridloghfry@gmail.com>
2024-09-15 13:18:58 +0700

    learn: php arrow function

---
+++ arrow_function.php
@@ -0,0 +1,13 @@
+<?php
+
+// arrow function similar to anonymous function but for simpler version
+// arrow function does not have to use "use" for accessing varibale outside closure
+// this feature only available on php version 7.4 or higher
+// arrow function uses fn keyword to define an arrow function
+
+$firstName = "ridlo";
+$lastName = "ghifary";
+
+$sayHello = fn() => "Hello $firstName $lastName";
+
+echo $sayHello() . PHP_EOL;
