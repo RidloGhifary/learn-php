09735d72299e1b3f07c5836eebf26e084798a447
RidloGhifary <ridloghfry@gmail.com>
2024-09-17 11:29:14 +0700

    learn: php oop function

--- oop/data/Person.php
+++ oop/data/Person.php
@@ -5,4 +5,9 @@ class Person
   var string $name;
   var ?string $city = null;
   var string $country = "Indonesia";
+
+  function sayHello(string $name)
+  {
+    echo "hello $name" . PHP_EOL;
+  }
 }
