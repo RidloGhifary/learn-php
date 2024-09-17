c57320aaa2c3995a6c415fb37ebb14b45cb20676
RidloGhifary <ridloghfry@gmail.com>
2024-09-17 14:18:06 +0700

    learn: contructor overriding

--- oop/data/Manager.php
+++ oop/data/Manager.php
@@ -3,6 +3,13 @@
 class Manager
 {
   var string $name;
+  var string $title;
+
+  public function __construct(string $name = "", string $title = "Manager")
+  {
+    $this->name = $name;
+    $this->title = $title;
+  }
 
   function sayHello(string $name)
   {
@@ -12,6 +19,11 @@ class Manager
 
 class VicePresident extends Manager
 {
+  public function __construct(string $name = '')
+  {
+    parent::__construct($name, "Vice President");
+  }
+
   function sayHello(string $name)
   {
     echo "Hi $name, My name is Vice President $this->name" . PHP_EOL;
