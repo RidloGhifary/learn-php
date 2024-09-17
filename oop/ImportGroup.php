16ab4e136815ddbefac266d242b934c37658a5e3
RidloGhifary <ridloghfry@gmail.com>
2024-09-17 13:05:52 +0700

    learn: php group use

---
+++ oop/ImportGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "./data/Conflict.php";
+require_once "./data/Helper.php";
+
+use Data\One\{Conflict, Sample, Dummy};
+use function Helper\{helpMe as Help, info as Info};
+
+Help();
+Info();
--- oop/data/Helper.php
+++ oop/data/Helper.php
@@ -8,4 +8,9 @@ function helpMe()
   echo "Help me" . PHP_EOL;
 }
 
+function info()
+{
+  echo "PHP@8.2" . PHP_EOL;
+}
+
 const APPLICATION = "BELAJAR PHP OOP";
