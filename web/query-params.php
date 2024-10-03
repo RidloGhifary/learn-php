5dfb07ed6a43d5dc26fc6c25796e158aa804b5b9
RidloGhifary <ridloghfry@gmail.com>
2024-10-03 17:16:40 +0700

    learn: php array query paramaters

--- web/query-params.php
+++ web/query-params.php
@@ -3,8 +3,22 @@
 $params = isset($_GET["first-name"]) && isset($_GET["last-name"])
   ? $_GET["first-name"] . " " . $_GET["last-name"]
   : null;
-// var_dump($_GET);
+
+$numbers = isset($_GET['numbers']) ? $_GET['numbers'] : [];
+$total = 0;
+$math = ""; // This will store the formula (e.g., 1 + 2 + 3 + 4)
+
+foreach ($numbers as $key => $number) {
+  $total += $number; // Sum the numbers
+  $math .= $number; // Append the number to the math string
+
+  if ($key < count($numbers) - 1) {
+    $math .= " + "; // Add a plus sign after each number except the last
+  }
+}
+
 $say = $params ? "Hello " . $params : "Hello World";
+error_log("Params: " . print_r($params, true));
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +31,8 @@ $say = $params ? "Hello " . $params : "Hello World";
 </head>
 
 <body>
-  <?= $say ?>
+  <p><?= $say ?></p>
+  <p><?= $math ?> = <?= $total ?></p>
 
   <script>
     // Use PHP to output the value in JavaScript
