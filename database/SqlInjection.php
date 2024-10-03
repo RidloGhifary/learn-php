bfa90633bb065fd179c1cdeb929a67b64f20ec08
RidloGhifary <ridloghfry@gmail.com>
2024-10-03 13:16:05 +0700

    learn: php sql injections solutions

--- database/SqlInjection.php
+++ database/SqlInjection.php
@@ -4,17 +4,24 @@ require_once __DIR__ . "/GetConnection.php";
 
 $connection = getConnection();
 
-$username = "admin'; #";
+$username = "admin";
 $password = "admin";
 
-$sql = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
+$sql = "SELECT * FROM admin WHERE username = :username AND password = :password";
 
-$statement = $connection->query($sql);
+$statement = $connection->prepare($sql);
+$statement->bindParam(":username", $username);
+$statement->bindParam(":password", $password);
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+var_dump($result);
 
 $success = false;
 $find_user = null;
 
-foreach ($statement as $row) {
+foreach ($result as $row) {
   // Success
   $success = true;
   $find_user = $row["username"];
