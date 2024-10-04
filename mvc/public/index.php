bea99f8b591865452a2da7258c05b58edd5e080f
RidloGhifary <ridloghfry@gmail.com>
2024-10-04 16:54:58 +0700

    learn: mvc basic router

--- mvc/public/index.php
+++ mvc/public/index.php
@@ -1,7 +1,9 @@
 <?php
 
+$path = "/index";
+
 if (isset($_SERVER['PATH_INFO'])) {
-  echo $_SERVER['PATH_INFO'];
-} else {
-  echo "There is no path info";
+  $path = $_SERVER['PATH_INFO'];
 }
+
+require __DIR__ . "/../app/View" . $path . ".php";
